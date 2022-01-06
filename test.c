438cdf8f1e450ab223cb4906a652dfb42288925b
Psarthex <lyerang@live.fr>
2022-01-06 23:28:09 +0100

    avc putchar

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 
+int ft_putchar(char c)
+{
+    write(1, &c, 26);
+}
 
 
 
 int     main()
 {
-    write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+    ft_putchar('abcdefghijklmnopqrstuvwyz');
     return (0);
 }

cfcc1452fcf32e83d88ee6397f3081490ac9a295
Psarthex <lyerang@live.fr>
2022-01-06 20:13:37 +0100

    mersi tibo

---
+++ 42/j02/ex00/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
+
+void     ft_print_alphabet(void)
+{
+    
+}
+
+int     main()
+{
+    ft_putchar()
+    ft_print_alphabet();
+    return (0);
+}

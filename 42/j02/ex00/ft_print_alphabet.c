90a17fe16b74ada2055c2c13cd2053fd6f108041
Psarthex <lyerang@live.fr>
2022-01-07 02:48:09 +0100

    slt c fini

--- 42/j02/ex00/ft_print_alphabet.c
+++ 42/j02/ex00/ft_print_alphabet.c
@@ -1,20 +1,33 @@
 #include <unistd.h>
-#include <stdio.h>
 
-int     ft_putchar(char c)
+int ft_putchar(char c)
 {
-    write(1, &c, 1);
-    return (0);
+     write(1, &c, 1);
+     return (0);
 }
 
-void     ft_print_alphabet(void)
+void ft_print_alphabet(void)
 {
-    
-}
+    char c;
+
+    c = 'a';
+
+
+        while (c <= 'z')
+        {
+            ft_putchar(c);
+            c++;
+        }
+
+    ft_putchar('\n');
+
+} 
+
+
 
 int     main()
 {
-    ft_putchar()
     ft_print_alphabet();
+
     return (0);
 }

96c4c86d0a27a52444dc52eb394e3d097a607e97
Psarthex <lyerang@live.fr>
2022-01-09 23:18:48 +0100

    J03 deja c fou..

--- 42/j02/ex04/ft_print_comb.c
+++ 42/j02/ex04/ft_print_comb.c
@@ -7,52 +7,35 @@ int     ft_putchar(char c)
 }
 void ft_print_comb(void)
 {
-   /// int tab[9];
-   ///int size = 789;
     char c;
     char d;
     char e;
 
-   /// tab[0] = 0;
-   /// tab[1] = 1;
-   /// tab[2] = 2;
-   /// tab[3] = 3;
-   /// tab[4] = 4;
-   /// tab[5] = 5;
-   /// tab[6] = 6;
-   /// tab[7] = 7;
-   /// tab[8] = 8;
-   /// tab[9] = 9;
     c = '0';
     d = '0';
     e = '0';
-   /// while (c <= size)
     while (c <= '9') 
+    {
+        while (d <= '9')
         {
-            while (d <= '9')
+            while (e <= '9')
             {
-                while (e <= '9')
-                {
-                    if(c <= d && d <= e)
-
-                        ft_putchar(c);
-                        ft_putchar(d);
-                        ft_putchar(e);
-                        if (c != '7')
-                        ft_putchar(',');
-                        if (c != '7')
-                        ft_putchar(' ');
-                        e++;
-                }
-                d++;
-                e = d + 1;
+                    ft_putchar(c);
+                    ft_putchar(d);
+                    ft_putchar(e);
+                if (c != '7')
+                    ft_putchar(',');
+                if (c != '7')
+                    ft_putchar(' ');
+                e++;
             }
-            c++;
-            d = c + 1;
+            d++;
             e = d + 1;
         }
-        
-    
+        c++;
+        d = c + 1;
+        e = d + 1;
+    }    
 }
 
 int     main()

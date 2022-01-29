2377a9ec4833c9ede52b5ef733ff926d17143c23
Psarthex <lyerang@live.fr>
2022-01-29 15:26:08 +0100

    p

--- 42/j03/ex08/ft_atoi.c
+++ 42/j03/ex08/ft_atoi.c
@@ -1,7 +1,29 @@
+#include <unistd.h>
 #include <stdio.h>
+
+int ft_putchar(char c)
+{
+    write(1, &c, 1);
+    return (0);
+}
 
 int     ft_atoi(char *str)
 {
+    int i;
+    int res;
+
+    i = 0;
+    res = 0;
+    while (str[i] != '\0')
+    {
+        res = res * 10 + str[i] - '0';
+        i++;
+    }
+    return res;
+
+}
+int main()
+{
+    char str[] = "123456";
     
 }
-

577395f7e34197ec897e5118b404a046067b3fa0
Psarthex <lyerang@live.fr>
2022-02-06 19:40:28 +0100

    push sort integer

--- 42/j03/ex09/ft_sort_integer_table.c
+++ 42/j03/ex09/ft_sort_integer_table.c
@@ -2,7 +2,24 @@
 
 void ft_sort_integer_tab(int *tab, int size)
 {
-    return tab;
+    int tmp;
+    int i;
+
+    i = 0;
+    while (i < size - 1)
+    {
+        if (tab[i] > tab[i + 1])
+        {
+            tmp = tab[i];
+            tab[i] = tab[i + 1];
+            tab[i + 1] = tmp;
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+    }
 }
 
 int main()

cb7b49ab95c49e5f6968d204c9757c3920b8fe60
Psarthex <lyerang@live.fr>
2022-02-07 15:51:46 +0100

    mobilep

--- 42/j05/ex02/ft_strstr.c
+++ 42/j05/ex02/ft_strstr.c
@@ -1,12 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+
 char    *ft_strstr(char *str, char *to_find)
 {
+    int i;
+    int j;
 
+    i = 0;
+    j = 0;
+    while(str[i] != '\0')
+    {
+        i++;
+    }
 }
 
 int main()
 {
     char s1[] = "i love my big sweatlord";
     char s2[] = "uwu sweatlord kun";
+    printf("%s\n", ft_strstr(s1, s2));
+    char s1[] = "i love my big sweatlord";
+    char s2[] = "huge cock";
+    printf("%s\n", ft_strstr(s1, s2));
+    char s1[] = "i love my big sweatlord";
+    char s2[] = "love";
+    printf("%s\n", ft_strstr(s1, s2));
+    char s1[] = "i love my big sweatlord";
+    char s2[] = "cringe";
+    printf("%s\n", ft_strstr(s1, s2));
+    char s1[] = "i love my big sweatlord";
+    char s2[] = "cringelord";
     printf("%s\n", ft_strstr(s1, s2));
     return(0);
 }

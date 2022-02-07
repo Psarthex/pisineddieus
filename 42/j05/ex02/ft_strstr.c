c99b6639b1fe743369237f5112fe01453c0697a4
Psarthex <lyerang@live.fr>
2022-02-07 12:37:05 +0100

    mobile push

--- 42/j05/ex02/ft_strstr.c
+++ 42/j05/ex02/ft_strstr.c
@@ -5,5 +5,8 @@ char    *ft_strstr(char *str, char *to_find)
 
 int main()
 {
-    
+    char s1[] = "i love my big sweatlord";
+    char s2[] = "uwu sweatlord kun";
+    printf("%s\n", ft_strstr(s1, s2));
+    return(0);
 }

ecd52d6364a6249feb0ff60a800280517578c217
Psarthex <lyerang@live.fr>
2022-02-07 16:00:42 +0100

    cringestrstr

--- 42/j05/ex02/ft_strstr.c
+++ 42/j05/ex02/ft_strstr.c
@@ -10,26 +10,39 @@ char    *ft_strstr(char *str, char *to_find)
     j = 0;
     while(str[i] != '\0')
     {
+        while(str[i] == to_find[j])
+        {
+            j++;
+            if (str[i] != to_find[j])
+            {
+                return (&str[i]);
+            }
+            i = 0;
+        }
         i++;
     }
+    return (&str[i]);
 }
 
 int main()
 {
     char s1[] = "i love my big sweatlord";
     char s2[] = "uwu sweatlord kun";
-    printf("%s\n", ft_strstr(s1, s2));
-    char s1[] = "i love my big sweatlord";
-    char s2[] = "huge cock";
-    printf("%s\n", ft_strstr(s1, s2));
-    char s1[] = "i love my big sweatlord";
-    char s2[] = "love";
-    printf("%s\n", ft_strstr(s1, s2));
-    char s1[] = "i love my big sweatlord";
-    char s2[] = "cringe";
-    printf("%s\n", ft_strstr(s1, s2));
-    char s1[] = "i love my big sweatlord";
-    char s2[] = "cringelord";
-    printf("%s\n", ft_strstr(s1, s2));
+    printf("ex1: %s\n", ft_strstr(s1, s2));
+    char s3[] = "i love my big sweatlord";
+    char s4[] = "huge cock";
+    printf("ex2: %s\n", ft_strstr(s3, s4));
+    char s5[] = "i love my big sweatlord";
+    char s6[] = "love";
+    printf("ex3: %s\n", ft_strstr(s5, s6));
+    char s7[] = "i love my big sweatlord";
+    char s8[] = "cringe";
+    printf("ex4: %s\n", ft_strstr(s7, s8));
+    char s9[] = "i love my big sweatlord";
+    char s10[] = "cringelord";
+    printf("ex5: %s\n", ft_strstr(s9, s10));
+    char s11[] = "i love my big sweatlord";
+    char s12[] = "drol";
+    printf("ex6: %s\n", ft_strstr(s11, s12));
     return(0);
 }
